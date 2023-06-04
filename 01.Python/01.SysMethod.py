6ff36027c20489094a886da4db26e915c0fa3ee5
jebinj4 <jebinj4@gmail.com>
2023-06-05 00:47:32 +0300

    Minor Update

---
+++ 01.Python/01.SysMethod.py
@@ -0,0 +1,9 @@
+# import sys
+# # `print(dir(sys))` will print out a list of all the attributes and methods available in the `sys`
+# # module.
+# # print(dir(sys))
+# # print(help(sys.getsizeof))
+# Cars_List=['a','b','c']
+# Cars_Tuples=('a','b','c')
+# print('List Memory Size:',sys.getsizeof(Cars_List))
+# print('Tuples Memory Size:',sys.getsizeof(Cars_Tuples))
